6662da2ff173edb97a8d91fd514262d98a9a241a
masterchiefff <anwarmagara@gmail.com>
2025-03-14 23:37:08 +0300

    fix issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const waitlistRoutes = require('./routes/waitlist');
+const waitlistRoutes = require('./routes/Waitlist');
 const errorHandler = require('./middleware/error');
 
 const app = express();
